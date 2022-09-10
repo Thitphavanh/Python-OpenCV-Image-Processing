75bad18e3f04fd2c627631cf30e1f8b11ba18409
Thitphavanh <thitphavanh@gmail.com>
2022-09-10 08:49:20 +0700

    video recorder

--- ep9_video_recorder.py
+++ ep9_video_recorder.py
@@ -3,18 +3,20 @@
 import cv2
 
 
-vdo = cv2.VideoCapture('images/Video.mp4')
+vdo = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+result = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
 
 while (vdo.isOpened()):
     check, frame = vdo.read()  # ຮັບພາບຈາກກ້ອງ frame ຕໍ່ frame
 
     if check == True:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Output', gray)
+        cv2.imshow('Output', frame)
+        result.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('e'):
             break
-    else:
-        break
 
+result.release()
 vdo.release()
 cv2.destroyAllWindows()
